e9911ba332ac96d710b09b3d94a44c85a0c79ed0
Richard Cruikshank <rcruiksh@uvic.ca>
2021-07-14 22:54:51 -0600

    final solution

--- solver.py
+++ solver.py
@@ -1,12 +1,10 @@
 # Richard Cruikshank V00844041
 
-# your program must
-# function correctly with an LP provided via standard input and generate its output to standard
-# output, with no command line parameters required for basic operation
-
 import sys
 import re
 
+# A node class used to represent an LP dictonary entry. This class allows us to
+# access information about the variables/coefficients directly
 class Node:
     def __init__(self, row, col, mag, number, dual_number):
         self.row = row
@@ -75,11 +73,13 @@ class LP:
 
         self.opt_var_numbering = [i for i in range(self.num_opt_vars)]
         self.slack_var_numbering = [i for i in range(self.num_opt_vars, self.num_opt_vars+self.num_constraints)]
-        # will not change - used to test optimalitcol
+
+        # will not change - used to test optimality
         self.nonbasic_var_indices = [(0,col) for col in range(1, len(self.matrix[0]))]
         self.basic_var_indices = [(row,0) for row in range(1,len(self.matrix))]
 
-        # use nodes to represent variables so we have more information about them??
+        # use nodes to represent variables so we have more information about them
+        # do this by converting our matrix of floats to a matrix of Nodes representing a dictionary
         self.dictionary = [[val for val in row] for row in self.matrix]
         for row in range(len(self.matrix)):
             for col in range(len(self.matrix[0])):
@@ -117,36 +117,19 @@ class LP:
         candidates = [node for node in self.dictionary[0][1:] if node.mag > 0]
         if len(candidates) == 0:
             candidates = [node for node in self.dictionary[0][1:] if node.mag >= 0]
-        #     print("RETURNING NONE")
-        #     return None
-        # else:
-        print("ENTERING CANDIDATES: ", candidates)
         candidate = min(candidates)
-        # print("Entering primal: " + str(candidate))
         return candidate
 
     def chooseEnteringDual(self):
         # get lowest numbered basic dual candidate
         candidates = [node for node in [row[0] for row in self.dictionary[1:]] if node.mag < 0]
-        print("DUAL CANDIDATES: ", candidates)
         if len(candidates) == 0:
             candidates = [node for node in [row[0] for row in self.dictionary[1:]] if node.mag <= 0]
         else:
             candidate = min(candidates)
-            # print("Entering dual: " + str(candidate))
             return candidate
 
     def chooseLeavingPrimal(self, enteringNode):
-
-        # candidates = (
-        #     [
-        #         (self.dictionary[i][0], self.dictionary[i][0].mag / (-1 * self.dictionary[i][enteringNode.col].mag))
-        #         for i in range(1, len(self.dictionary))
-        #         if self.dictionary[i][0].mag > 0
-        #         and self.dictionary[i][enteringNode.col].mag < 0
-        #     ]
-        # )
-
         # compute the bounds on the candidate leaving variables
         candidates = (
             [
@@ -157,7 +140,6 @@ class LP:
             ]
         )
         if len(candidates) == 0:
-            # print("LENGTH ZERO")
             candidates = (
                 [
                     (i, self.dictionary[i][0].mag / (-1 * self.dictionary[i][enteringNode.col].mag))
@@ -166,20 +148,13 @@ class LP:
                     and self.dictionary[i][enteringNode.col].mag < 0
                 ]
             )
-        print("CANDIDATES: ", candidates)
         minimum = min(pair[1] for pair in candidates)
-        # print("minimum: ", minimum)
         candidates = [pair for pair in candidates if pair[1] == minimum]
-        # print("CANDIDATES2: ", candidates)
-        print("LEAVING CANDIDATES: ", candidates)
         if len(candidates) == 1:
-            # print("len 1: ", self.dictionary[candidates[0][0]][0])
             return self.dictionary[candidates[0][0]][0]
         elif len(candidates) > 1:
             candidates = [self.dictionary[candidate[0]][0] for candidate in candidates]
             candidates.sort()
-            # print(candidates)
-            # print("len > 1: ", candidates[0])
             return candidates[0]
         return
 
@@ -193,8 +168,9 @@ class LP:
                 and self.dictionary[enteringNode.row][i].mag > 0
             ]
         )
+        if len(candidates) == 0:
+            return "infeasible"
         minimum = min(pair[1] for pair in candidates)
-        print(minimum)
         candidates = [pair for pair in candidates if pair[1] == minimum]
         if len(candidates) == 1:
             return self.dictionary[0][candidates[0][0]]
@@ -205,17 +181,15 @@ class LP:
 
     def primalPivot(self):
         entering = self.chooseEnteringPrimal()
-        print("Entering: " + str(entering))
         if (self.isPrimalUnbounded(entering)):
-            print("unbounded")
             return "unbounded"
         leaving = self.chooseLeavingPrimal(entering)
-        print("Leaving: " + str(leaving))
 
         entering_number = entering.number
         leaving_number = leaving.number
         entering_dual_number = entering.dual_number
         leaving_dual_number = leaving.dual_number
+
         # solve leaving var row for entering var
         leaving_row = self.dictionary[leaving.row]
         leaving_coeff = leaving_row[entering.col] * -1
@@ -238,43 +212,30 @@ class LP:
         self.dictionary[leaving.row][leaving.col].number = entering_number
         self.dictionary[entering.row][entering.col].dual_number = leaving_dual_number
         self.dictionary[leaving.row][leaving.col].dual_number = entering_dual_number
-        # self.printDictionary()
         return ""
 
     def dualPivot(self):
         entering = self.chooseEnteringDual()
-        print("Entering: " + str(entering))
         if (self.isPrimalUnbounded(entering)):
-            print("unbounded")
             return "unbounded"
         leaving = self.chooseLeavingDual(entering)
-        print("Leaving: " + str(leaving))
+        if leaving == "infeasible":
+            return "infeasible"
 
         entering_number = entering.number
         leaving_number = leaving.number
         entering_dual_number = entering.dual_number
         leaving_dual_number = leaving.dual_number
+
         # solve leaving var row for entering var
         leaving_col = [row[leaving.col] for row in self.dictionary]
         leaving_coeff = leaving_col[entering.row]
-        # print("leaving coeff: " + str(leaving_coeff))
-        # self.printDictionary()
         leaving_col[entering.row] = Node(leaving.col, entering.row, 1, float('inf'), float('inf'))
-        # self.printDictionary()
-
-        # print("leaving coeff: " + str(leaving_coeff))
-        # for i in range(1, len(leaving_row)):
-            # leaving_row[i] = leaving_row[i] / leaving_coeff
         leaving_col = [val/leaving_coeff for val in leaving_col]
-        # print("LEAVING ROW")
-        # print(leaving_row)
         for row in range(len(self.dictionary)):
             self.dictionary[row][leaving.col] = leaving_col[row]
-        # self.printDictionary()
-        # objective row:
-        # coeff = self.dictionary[row][entering.col].mag
-        # expansion = [coeff * val.mag for val in leaving_row]
 
+        # compute other rows
         for col in range(self.num_cols):
             if col != leaving.col:
                 coeff = self.dictionary[entering.row][col].mag * -1
@@ -293,7 +254,6 @@ class LP:
         self.dictionary[leaving.row][leaving.col].number = entering_number
         self.dictionary[entering.row][entering.col].dual_number = leaving_dual_number
         self.dictionary[leaving.row][leaving.col].dual_number = entering_dual_number
-        # self.printDictionary()
         return ""
 
     def generateAuxMatrix(self):
@@ -301,21 +261,17 @@ class LP:
 
     def isPrimalFeasible(self):
         neg_basic_vals = [i for i in self.getBasicVarVals() if i < 0]
-        # print("PRIMAL_VARS: ", neg_basic_vals)
         return len(neg_basic_vals) == 0
 
     def isDualFeasible(self):
         pos_nonbasic_vals = [i for i in self.getNonBasicVarVals() if i > 0]
-        # print("DUAL_VARS: ", pos_nonbasic_vals)
         return len(pos_nonbasic_vals) == 0
 
     def isOptimal(self):
-        # print("primal feasible: ", self.isPrimalFeasible())
-        # print("dual feasible: ", self.isDualFeasible())
         return self.isPrimalFeasible() and self.isDualFeasible()
 
     def isPrimalUnbounded(self, entering):
-        # one or moreone or more possible entering variables has
+        # one or more possible entering variables has
         # no upper bound (that is, if the coefficient for that variable
         # in every basis row is positive or zero)
         leaving_coeffs = [row[entering.col] for row in self.dictionary[1:] if row[entering.col].mag < 0]
@@ -325,73 +281,48 @@ class LP:
         leaving_coeffs = [col for col in self.dictionary[entering.row] if col.mag > 0]
         return len(leaving_coeffs) == 0
 
-    def isInfeasible(self):
-        # run dual simplex, if unbounded then infeasible.
-        # otherwise, use optimal basis with primal simplex to solve
-        return
-
     def solve(self):
         res = ""
         if self.isOptimal():
-            print("optimal")
-            print("{:.7g} ".format(self.getObjectiveVal()))
-            for entry in self.getOptVarVals():
-                print("{:.7g} ".format(entry), end="")
-            print()
+            res = "optimal"
+            # print("optimal")
+            # print("{:.7g} ".format(self.getObjectiveVal()))
+            # for entry in self.getOptVarVals():
+            #     print("{:.7g} ".format(entry), end="")
+            # print()
+
         elif self.isPrimalFeasible():
-            print("primal feasible")
             while not self.isOptimal() and res != "unbounded":
                 res = self.primalPivot()
-                print("OBJECTIVE: ", self.getObjectiveVal())
-                # self.printDictionary()
-                # print("nonbasic vars: ", self.getNonBasicVars())
-                # print("basic vars: ", self.getBasicVars())
-            # self.printDictionary()
+
         elif self.isDualFeasible():
-            print("dual feasible")
-            while not self.isOptimal() and res != "unbounded":
+            while not self.isOptimal() and res != "unbounded" and res != "infeasible":
                 res = self.dualPivot()
-                print("OBJECTIVE: ", self.getObjectiveVal())
-                # print("nonbasic vars: ", self.getNonBasicVars())
-                # print("basic vars: ", self.getBasicVars())
+
         else: # initially infeasible
-            print("Initially infeasible")
             og_objective = [Node(col.row, col.col, col.mag, col.number, col.dual_number) for col in self.dictionary[0]]
-            print("OG", og_objective)
 
             for col in self.dictionary[0]:
                 col.mag = 0
 
-            print("OG", og_objective)
-            self.printDictionary()
-
-            print("dual_feas? ", self.isDualFeasible())
-            print("primal_feas? ", self.isPrimalFeasible())
-
-            while not self.isOptimal() and res != "unbounded":
+            while not self.isOptimal() and res != "unbounded" and res != "infeasible":
                 res = self.dualPivot()
-                print("OBJECTIVE: ", self.getObjectiveVal())
-                self.printDictionary()
-            if res == "unbounded":
+            if res == "unbounded" or res == "infeasible":
                 res = "infeasible"
             else:
-                print("OPTIMAL")
-                self.printDictionary()
                 new_objective = []
-                # basic vars
                 for node in og_objective:
                     for row in self.dictionary:
                         if row[0].number == node.number:
-                            print("wawaweewa")
                             new_objective.append([Node(col.row, col.col, col.mag * node.mag, col.number, col.dual_number) for col in row])
+
                 n_o = new_objective[0]
-                print("new_objective:", new_objective)
+                # basic vars
                 if len(new_objective) > 1:
-                    print("new_objective:", new_objective[1:])
                     for row in new_objective[1:]:
                         for i in range(len(row)):
-                            print("BRO")
                             n_o[i] += row[i]
+
                 # nonbasic vars
                 for node in og_objective:
                     if node.number in [col.number for col in self.dictionary[0][1:]]:
@@ -399,28 +330,12 @@ class LP:
                             if node.number == self.dictionary[0][col].number:
                                 n_o[col] += node
 
-                print("n_o: ", n_o)
                 self.dictionary[0] = n_o
-                self.printDictionary()
                 res = self.solve()
-                self.printDictionary()
-
-            print("RESULT: ", res)
-
 
         if self.isOptimal():
-            print("optimal")
-            print("{:.7g} ".format(self.getObjectiveVal()))
-            for entry in self.getOptVarVals():
-                print("{:.7g} ".format(entry), end="")
-            print()
+            res = "optimal"
 
-        # if (initially_primal_feasible):
-        #     print("INITIALLY PRIMAL FEASIBLE")
-        # elif (initially_dual_feasible):
-        #     print("INITIALLY DUAL FEASIBLE")
-        # else:
-        #     print("man fuck I don't wanna solve this")
         return res
 
     def printMatrix(self):
@@ -484,20 +399,15 @@ def parseInput():
 
 def main():
     A = LP(parseInput())
-    # A.printMatrix()
-    print()
-    # A.printDictionary()
-    print("solving")
-    A.solve()
-    # print("\nmatrix: {}".format(A.matrix))
-    # print("num_opt_vars: {}".format(A.num_opt_vars))
-    # print("\nobjective_val: {}".format(A.objective_val))
-    # print("\nobjective_row: {}".format(A.objective_row))
-    # print("\nbasis: {}".format(A.basis))
-    # print("\nopt_var_indices: {}".format(A.opt_var_indices))
-    # print("\nopt var vals: {}".format(A.getOptVarVals()))
-    # print("\nslack_var_indices: {}".format(A.slack_var_indices))
-    # print("\nslack var vals: {}".format(A.getSlackVarVals()))
+    result = A.solve()
+    if result == "optimal":
+        print("optimal")
+        print("{:.7g} ".format(A.getObjectiveVal()))
+        for entry in A.getOptVarVals():
+            print("{:.7g} ".format(entry), end="")
+        print()
+    else:
+        print(result)
     return
 
 if __name__ == "__main__":
